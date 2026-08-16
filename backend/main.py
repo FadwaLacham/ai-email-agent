d360570dc9ddb5cd57dedef555d38911dff1f9c7
Fadwa Lacham <fadwa.lacham@gmail.com>
2026-08-16 18:14:43 +0200

    Configure FastAPI Cloud deployment

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.database.database import Base, engine
 from app.database import models
 from app.auth.routes import router as auth_router
-
 from app.api.routes import router
 
 
@@ -17,27 +16,16 @@ app = FastAPI(
 # =========================
 # CORS Configuration
 # =========================
-app.include_router(
-    auth_router,
-    prefix="/auth",
-    tags=["Authentication"]
-)
-
 app.add_middleware(
-
     CORSMiddleware,
-
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://ai-email-agent-dashboard.netlify.app"
     ],
-
     allow_credentials=True,
-
     allow_methods=["*"],
-
     allow_headers=["*"],
-
 )
 
 
@@ -45,13 +33,17 @@ app.add_middleware(
 # Routes
 # =========================
 
-app.include_router(router)
+app.include_router(
+    auth_router,
+    prefix="/auth",
+    tags=["Authentication"]
+)
 
+app.include_router(router)
 
 
 @app.get("/")
 def home():
-
     return {
         "message": "AI Email Agent API running"
     }
